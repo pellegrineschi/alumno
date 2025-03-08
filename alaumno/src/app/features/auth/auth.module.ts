5b80942d03acfa972c5957c55b1ef6be7a6d7d6f
pellegrineschi <pellegrineschi86@gmail.com>
2025-03-08 17:48:14 -0300

    login

--- alaumno/src/app/features/auth/auth.module.ts
+++ alaumno/src/app/features/auth/auth.module.ts
@@ -9,6 +9,7 @@ import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
 import {MatSelectModule} from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
 
 
 @NgModule({
@@ -23,7 +24,8 @@ import {MatSelectModule} from '@angular/material/select';
     MatFormFieldModule,
     MatInputModule,
     MatIconModule,
-    MatSelectModule
+    MatSelectModule,
+    MatButtonModule
 
   ]
 })
